396f7c0ff2b492b2de7b3405edc78a37c486484c
yamank41 <100734289+yamank41@users.noreply.github.com>
2022-03-19 10:37:50 +0300

    i made the struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,10 @@
 import UIKit
 
 var greeting = "Hello, playground"
+struct Student{
+    let name : String
+    let age : Int
+    let grade : Double
+    let attendence : Bool
+    }
+var ahmed : Student = Student(name: "ahmed", age: 16, grade: 100.0, attendence:true )
